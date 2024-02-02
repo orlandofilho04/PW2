da91b51fcfbc13baa26a4d713c0f968f25c6d203
Orlando Filho <orlandossf04@gmail.com>
2024-02-01 21:53:33 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,8 +1,18 @@
-<?php 
-    echo"teste";
-    $b = true;
-    $num = 1;
-    $text = "desgraça";
-    $fracao = 3.5;
-    var_dump ($b,$num,$text,$fracao);
-?>
+<?php
+echo "teste";
+$b = true;
+$num = 1;
+$text = "desgraça";
+$fracao = 3.5;
+
+$array1 = ["lepo", "torna", "vapo"];
+
+$array2 = [
+    ["lepo" => "nossa"],
+    ["torna" => "chaves"],
+    ["vapo" => "douglas"]
+];
+
+echo "inteiro: ($num)/n";
+
+var_dump($b, $num, $text, $fracao, $array1, $array2);
