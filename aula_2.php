7fed639ba2c487ebf0083501ead29b0c6d69709a
Orlando Filho <orlandossf04@gmail.com>
2024-02-08 20:00:24 -0300

    ex

---
+++ aula_2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    $a = 10;
+    $b = 5;
+
+    $ad = $a + $b;
+    $sb = $a - $b;
+    $mt = $a * $b;
+    $dv = $a / $b;
+
+    var_dump([$ad, $sb, $mt, $dv]);
+
+    ?>
+
+</body>
+
+</html>
