c30a59476053f458ba5cede78373d46277d60ee2
Orlando Filho <orlandossf04@gmail.com>
2024-02-22 19:25:59 -0300

    update

--- pgweb/index.php
+++ pgweb/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
     <?php
@@ -27,6 +29,7 @@
 
     echo "<br>O maior é = {$maior}, menor = {$menor}";
     ?>
-    
+
 </body>
+
 </html>
