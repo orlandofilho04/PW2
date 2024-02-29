f91dd3c45efd646b644cc89b875fc1ea339b6145
Orlando Filho <orlandossf04@gmail.com>
2024-02-29 19:43:06 -0300

    add rep

---
+++ estrut_repet/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Repetição</title>
+</head>
+
+<body>
+    <h1>Estrutura de Repetição</h1>
+    <h2>Exemplo 01 - Contador</h2>
+    <?php
+
+    $x = 1;
+
+    while ($x <= 10) {
+        echo "O valor de x é: $x <br>";
+        $x++;
+    }
+
+    ?>
+
+</body>
+
+</html>
