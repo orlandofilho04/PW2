476eb890769961a498991ff465650bc51a76d559
Orlando Filho <orlandossf04@gmail.com>
2024-02-29 20:10:17 -0300

    att

--- estrut_repet/dowhile.php
+++ estrut_repet/dowhile.php
@@ -4,35 +4,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Repetição</title>
+    <title>Repetição do while</title>
 </head>
 
 <body>
-    <h1>Estrutura de Repetição</h1>
+    <h1>Estrutura de Repetição dowhile</h1>
     <h2>Exemplo 01 - Contador</h2>
     <?php
 
-    $x = 1;
+    $x = 0;
 
-    while ($x <= 10) {
+    do {
         echo "O valor de x é: $x <br>";
         $x++;
-    }
+    } while ($x <= 10);
 
     ?>
 
     <h2>Exemplo 02 - </h2>
 
-    <?php   
+    <?php
     $array = ["maçã", "pera", "uva", "banana", "laranja"];
-
+    //$array = [];
     echo "O array tem " . count($array) . " elementos <br> <br>";
 
     $i = 0;
-    while ($i < count($array)) {
-        echo "O valor do array na posição $i é: $array[$i] <br>";
-        $i++;
-    }
+    do {
+        if (count($array) > 0) {
+            echo "O valor do array na posição $i é: $array[$i] <br>";
+            $i++;
+        } else {
+            echo "O array está vazio";
+        }
+
+    } while ($i < count($array));
     ?>
 
 </body>
